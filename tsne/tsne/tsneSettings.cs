0c7a72c39da5490152e44c2dc679fd7000a37253
fel88 <15663687+fel88@users.noreply.github.com>
2024-07-04 00:33:04 +0300

    initial

---
+++ tsne/tsne/tsneSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace tsneDemo
+{
+    public class tsneSettings
+    {
+        public double epsilon = 10; // epsilon is learning rate (10 = default)
+        public double perplexity = 30; // roughly how many neighbors each point influences (30 = default)
+        public int dim = 2; // dimensionality of the embedding (2 = default)
+    }
+}
